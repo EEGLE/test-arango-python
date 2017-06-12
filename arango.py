6cc5cbff97756ced57f25d6c772386f4f3fdacaf
Titouan CREACH <titouan.creach@eegle.io>
2017-06-12 11:03:42 +0200

    add python script

---
+++ arango.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from pyArango.connection import *
+
+conn = Connection(username='root')
+
+db = conn['eegleio']
+
+try:
+    userCollection = db.createCollection(name='Users')
+except Exception as e:
+    userCollection = db['Users']
+
+GET_USER_QUERY = """
+    FOR u in Users
+        FILTER u.email == "titouan.creach@eegle.io"
+        RETURN {name: u.name, key: u._key}
+"""
+
+document = userCollection.createDocument()
+
+document['name'] = 'titouan'
+document['email'] = 'titouan.creach@eegle.io'
+
+document.save()
+
+result = db.AQLQuery(GET_USER_QUERY, rawResults=True)
+
+for user in result:
+    print('name: {}, key: {}'.format(user['name'], user['key']))
+
+
